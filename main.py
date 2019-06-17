be7bcaab052847d95e816e5bd2c3c7dad11e78d1
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-06-17 20:10:39 +0300

    [FIX] Stupid fixes T_t

--- main.py
+++ main.py
@@ -145,6 +145,6 @@ def stat():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
+    app.debug = True
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
